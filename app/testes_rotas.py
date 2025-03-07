bc29e6cf0396b571b318b0c7365bf3da96096c68
wesscav <wesleyalvescav@gmail.com>
2025-03-07 15:32:58 -0300

    criação da conexao com o banco de dados no arquivo de testes
    Co-authored-by:Matheus Luciano <matheuslaos@al.insper.edu.br>

---
+++ app/testes_rotas.py
@@ -0,0 +1,22 @@
+import pytest
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+service_URI = os.getenv("SERVICE_URI")
+database_name = os.getenv("DATABASE_NAME")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+
+@pytest.fixture
+def conexao():
+    return mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database_name
+    )
+
