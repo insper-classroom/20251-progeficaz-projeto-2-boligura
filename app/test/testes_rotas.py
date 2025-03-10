c0b7aa2c214a5a81702fddfa0fdd0a2cc4a618f2
matheusluciano99 <matheusluciano99@gmail.com>
2025-03-10 18:43:45 -0300

    alterando variaveis do database

--- app/test/testes_rotas.py
+++ app/test/testes_rotas.py
@@ -5,16 +5,16 @@ import mysql.connector
 
 load_dotenv()
 
-service_URI = os.getenv("SERVICE_URI")
-database_name = os.getenv("DATABASE_NAME")
-host = os.getenv("HOST")
-port = os.getenv("PORT")
-user = os.getenv("USER")
-password = os.getenv("PASSWORD")
+SERVICE_URI = os.getenv("SERVICE_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
 
 
 @pytest.fixture
 def conexao():
     return mysql.connector.connect(
-        host=host, user=user, password=password, database=database_name
+        host=HOST, user=USER, password=PASSWORD, database=DATABASE_NAME
     )
