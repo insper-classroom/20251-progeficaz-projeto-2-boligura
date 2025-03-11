ebd5e5bf8502fb022438d16a0df327e32c881a9b
wesscav <wesleyalvescav@gmail.com>
2025-03-11 08:21:51 -0300

    criação do teste de adicionar_imovel

--- app/test_servidor.py
+++ app/test_servidor.py
@@ -27,3 +27,26 @@ def test_obter_imovel_por_id(conexao, imovel_teste):
     assert imovel is not None
     assert imovel["id"] == imovel_teste
     assert imovel["logradouro"] == "Rua de Teste"
+
+def test_adicionar_imovel(client):
+    novo_imovel = {
+        "logradouro": "Avenida Teste",
+        "numero": 123,
+        "cidade": "Cidade Teste",
+        "estado": "TS",
+        "preco": 250000.00,
+        "tipo": "Apartamento"
+    }
+    
+    response = client.post("/imoveis", json=novo_imovel)
+    assert response.status_code == 201  # Código HTTP para criação bem-sucedida
+    
+    dados_resposta = response.json
+    assert "id" in dados_resposta  # Verifica se um ID foi retornado
+    assert dados_resposta["logradouro"] == novo_imovel["logradouro"]
+    assert dados_resposta["numero"] == novo_imovel["numero"]
+    assert dados_resposta["cidade"] == novo_imovel["cidade"]
+    assert dados_resposta["estado"] == novo_imovel["estado"]
+    assert dados_resposta["preco"] == novo_imovel["preco"]
+    assert dados_resposta["tipo"] == novo_imovel["tipo"]
+
